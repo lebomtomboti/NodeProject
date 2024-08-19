3244faec59128324811cf7206d194be8d5d7601c
Kifaa26 <akifahpalekar@gmail.com>
2024-08-19 15:47:11 +0200

    Nav and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar />
+  <main>
+    <router-view />
+  </main>
+  <Footer/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+NavBar,
+Footer
 }
+</script>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style src="./assets/css/style.css">
+
 </style>
